6fa03e8faeed10d263743c09adf24a0ffa790b2b
Minh Tri Nguyen Vo <89735910+minhtringuyen31@users.noreply.github.com>
2022-07-13 14:43:07 +0700

    first commit

---
+++ setting.py
@@ -0,0 +1,17 @@
+# Define specifications
+
+ # Set the window size
+WIDTH  = 600 
+HEIGHT = 600
+WIDTH_SCREEN = 600
+# Set line color
+LINE_COLOR = (23, 145, 135)
+
+LINE_WIDTH = 15
+OFFSET = 15
+# Set image of object
+IMAGE_0 = 'o.png'
+IMAGE_X = 'x.png'
+IMAGE_BG = 'bg.jpg'
+SOUND_BUTTON = 'bt1.wav'
+INF = 0 
