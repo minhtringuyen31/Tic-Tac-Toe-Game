95f60ea5cafc15e8571129011bdeaa952cf85450
Minh Tri Nguyen Vo <89735910+minhtringuyen31@users.noreply.github.com>
2022-07-13 14:47:18 +0700

    main

---
+++ main.py
@@ -0,0 +1,18 @@
+# Class 20CLC01 - Term III/2021-2022
+# Course: CSC14003 - Artificial Intelligence
+# PROJECT 01: SERCH
+# TIC-TAC-TOE Game
+
+# Hoang Huu Minh An  - 20127102
+# Nguyen Vo Minh Tri - 20127364
+# Phan Minh Xuan     - 20127395
+
+from game import Game
+# Main function initialize a game object 
+# RUN GAME
+def main():
+    game = Game()
+    game.run()
+
+if __name__ == '__main__':
+    main()
